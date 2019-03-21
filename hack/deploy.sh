dc93c84f7931ceb254e514e4cd048e72d1e8fd0b
Jeffrey Sica <jeef111x@gmail.com>
2019-03-21 10:54:35 -0400

    fix multiarch build/pushing

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -6,22 +6,28 @@ manifest_list="";
 
 
 for i in ${arch_list}; do
-    container="${manifest}-${i}"
+    # If it's a tagged release, use the tag
+    # Otherwise, assume it's HEAD and push to latest
+    container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
     
-    echo "Now building ${container}"
-    echo "docker build --no-cache --build-arg GOARCH=${i} -t ${container} .";
+    echo "--- docker build --no-cache --build-arg GOARCH=${i} -t ${container} .";
     docker build --no-cache --build-arg GOARCH=${i} -t ${container} .
 
-    echo "Now pushing ${container}"
+    echo "--- docker push ${container}"
     docker push ${container}
     
-    manifest_list="${manifest_list} ${manifest}-${i}"
+    manifest_list="${manifest_list} ${container}"
 done;
 
+echo "--- docker manifest create --amend ${manifest} ${manifest_list}"
 docker manifest create --amend ${manifest} ${manifest_list}
 
 for i in ${arch_list}; do
-    docker manifest annotate ${manifest} "${manifest}-${i}" --os linux --arch ${i}
+    container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
+
+    echo "--- docker manifest annotate ${manifest} ${container} --os linux --arch ${i}"
+    docker manifest annotate ${manifest} ${container} --os linux --arch ${i}
 done;
 
+echo "--- docker manifest push ${manifest}"
 docker manifest push ${manifest}
