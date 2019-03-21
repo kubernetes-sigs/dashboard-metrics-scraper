e1f10cbc3f80ae94d55c5e2a239d273a9b7a0a63
Jeffrey Sica <jeef111x@gmail.com>
2019-03-20 21:49:34 -0400

    start ci pipeline

--- hack/push-images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in amd64 arm armv7; do
-    container="metrics-sidecar-${i}"
-    echo "Now pushing ${container}"
-    docker push --build-arg GOARCH=${i} -t ${container} .
-done;
