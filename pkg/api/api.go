ca2814ae2759ad8e2c1799cff2485e8dbf148c33
Brad Davidson <brad@oatmail.org>
2019-07-05 23:22:57 +0000

    Log output enhancements:
    * Allow setting log level on the command line
    * Log all requests to stdout; not just requests to the default/root handler

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -5,30 +5,18 @@ import (
 	"fmt"
 	"net/http"
 
-	log "github.com/sirupsen/logrus"
-
 	"github.com/gorilla/mux"
 	dashboardProvider "github.com/kubernetes-sigs/dashboard-metrics-scraper/pkg/api/dashboard"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func ApiManager(r *mux.Router, db *sql.DB) {
-	r.HandleFunc("/", RootHandler)
 	dashboardRouter := r.PathPrefix("/api/v1/dashboard").Subrouter()
 	dashboardProvider.DashboardRouter(dashboardRouter, db)
-
 	r.PathPrefix("/").HandlerFunc(DefaultHandler)
-
-}
-
-func RootHandler(w http.ResponseWriter, r *http.Request) {
-	msg := fmt.Sprintf("URL: %s", r.URL)
-	log.Info(msg)
-	w.Write([]byte(msg))
 }
 
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	msg := fmt.Sprintf("URL: %s", r.URL)
-	log.Info(msg)
 	w.Write([]byte(msg))
 }
