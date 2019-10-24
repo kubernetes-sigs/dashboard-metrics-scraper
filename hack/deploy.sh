1310bcee1b46c1c01ea2cd39b53803c4b6e2c4ed
Sebastian Florek <sebastian.florek@loodse.com>
2019-10-24 12:12:38 +0200

    Fix scraper deploy

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -9,25 +9,25 @@ for i in ${arch_list}; do
     # If it's a tagged release, use the tag
     # Otherwise, assume it's HEAD and push to latest
     container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
-    
+
     echo "--- docker build --no-cache --build-arg GOARCH=${i} -t ${container} .";
     docker build --no-cache --build-arg GOARCH=${i} -t ${container} .
 
     echo "--- docker push ${container}"
     docker push ${container}
-    
+
     manifest_list="${manifest_list} ${container}"
 done;
 
-echo "--- docker manifest create --amend ${manifest} ${manifest_list}"
-docker manifest create --amend ${manifest} ${manifest_list}
+echo "--- docker manifest create --amend ${manifest}:${TRAVIS_TAG:="latest"} ${manifest_list}"
+docker manifest create --amend ${manifest}:${TRAVIS_TAG:="latest"} ${manifest_list}
 
 for i in ${arch_list}; do
     container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
 
-    echo "--- docker manifest annotate ${manifest} ${container} --os linux --arch ${i}"
-    docker manifest annotate ${manifest} ${container} --os linux --arch ${i}
+    echo "--- docker manifest annotate ${manifest}:${TRAVIS_TAG:="latest"} ${container} --os linux --arch ${i}"
+    docker manifest annotate ${manifest}:${TRAVIS_TAG:="latest"} ${container} --os linux --arch ${i}
 done;
 
-echo "--- docker manifest push ${manifest}"
-docker manifest push ${manifest}
+echo "--- docker manifest push ${manifest}:${TRAVIS_TAG:="latest"}"
+docker manifest push ${manifest}:${TRAVIS_TAG:="latest"}
