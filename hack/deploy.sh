2209355707095b734aad9039aff4c21c403e5d11
James Tumber <james.tumber@ibm.com>
2023-01-10 13:11:49 +0000

    Use buildx for cross-platform builds
    Use Docker buildx build tooling to create multi-arch manifest instead of manually adding images.
    
    Test build:
    https://hub.docker.com/r/jamesnelson/metrics-scraper/tags
    
    Signed-off-by: James Tumber <james.tumber@ibm.com>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-arch_list="amd64 arm arm64 ppc64le s390x"
+arch_list=(amd64 arm arm64 ppc64le s390x)
 manifest="kubernetesui/metrics-scraper";
-manifest_list="";
 
+# Concat arch_list with ,
+platforms=$(IFS=,; echo "${arch_list[*]}")
 
-for i in ${arch_list}; do
-    # If it's a tagged release, use the tag
-    # Otherwise, assume it's HEAD and push to latest
-    container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
+image_name="${manifest}:${TRAVIS_TAG:="latest"}"
 
-    echo "--- docker build --no-cache --build-arg GOARCH=${i} -t ${container} .";
-    docker build --no-cache --build-arg GOARCH=${i} -t ${container} .
-
-    echo "--- docker push ${container}"
-    docker push ${container}
-
-    manifest_list="${manifest_list} ${container}"
-done;
-
-echo "--- docker manifest create --amend ${manifest}:${TRAVIS_TAG:="latest"} ${manifest_list}"
-docker manifest create --amend ${manifest}:${TRAVIS_TAG:="latest"} ${manifest_list}
-
-for i in ${arch_list}; do
-    container="${manifest}-${i}:${TRAVIS_TAG:="latest"}"
-
-    echo "--- docker manifest annotate ${manifest}:${TRAVIS_TAG:="latest"} ${container} --os linux --arch ${i}"
-    docker manifest annotate ${manifest}:${TRAVIS_TAG:="latest"} ${container} --os linux --arch ${i}
-done;
-
-echo "--- docker manifest push ${manifest}:${TRAVIS_TAG:="latest"}"
-docker manifest push ${manifest}:${TRAVIS_TAG:="latest"}
+echo "--- docker buildx build --push --platform $platforms --tag $image_name .";
+docker buildx build --push --platform ${platforms} --tag ${image_name} .
